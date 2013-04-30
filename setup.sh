21e674f7d22a81a2d2b67bcbae48fd7b5e0bb09b
Dustin Kut Moy Cheung <thescouser_89@yahoo.co.uk>
2013-04-30 14:01:14 -0400

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# create the symlink for vimrc
+ln -s `pwd`/vimrc ~/.vimrc
+
+mkdir -p bundle/vundle
+git clone https://github.com/gmarik/vundle.git bundle/vundle
+
+echo "Please run *vim* and type:"
+echo ""
+echo "  :BundleInstall"
+echo ""
+echo ""
+echo "This will install all the vim plugins"
